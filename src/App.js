bbf14098cc011bace02ad356ff65c5ea181f3bb7
PortgasD.Adem <adembessam@gmail.com>
2024-01-17 15:11:24 -0500

    fixed bugs

--- src/App.js
+++ src/App.js
@@ -2,11 +2,9 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import HomePage from './Pages/HomePage';
-import MyNavBar from "/Users/adembessam/Desktop/Projet personnel/gabi./src/Components/MyNavBar.js";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Albums from "./Pages/Albums";
-import Footer from "./Pages/Footer";
 
 
 export default function App() {
