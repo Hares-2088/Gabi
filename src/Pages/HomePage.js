8bdcdf124b4d7e40dc2978366f4822dcaf717fe2
PortgasD.Adem <adembessam@gmail.com>
2024-10-09 10:49:48 -0400

    made the app more reusable

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,39 +1,74 @@
-import React, { useEffect } from 'react'
-import AlbumList from '../Components/AlbumList'
+import React, { useEffect, useState } from 'react';
+import AlbumList from '../Components/AlbumList';
+import { parseString } from 'xml2js';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleDown } from '@fortawesome/free-regular-svg-icons'
-import anime from 'animejs/lib/anime.es.js';
-import { faEnvelope, } from '@fortawesome/free-solid-svg-icons'
-import { faFacebook, faInstagram, faSpotify, faTwitter } from '@fortawesome/free-brands-svg-icons'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faFacebook, faInstagram, faSpotify, faTwitter } from '@fortawesome/free-brands-svg-icons';
 
 export default function HomePage() {
+  const [artist, setArtist] = useState(null);
 
-  // const scrollToBottom = () => {
-  //   window.scrollTo({
-  //     top: document.body.scrollHeight,
-  //     behavior: 'smooth',
-  //   });
-  // };
+  const fetchXmlData = async () => {
+    try {
+      const response = await fetch('/albums.xml');
+      const xmlData = await response.text();
+
+      parseString(xmlData, (err, result) => {
+        if (err) {
+          console.error('Error parsing XML:', err);
+        } else {
+          const artistData = result?.Library?.Artist?.[0];  // Fetch the first artist object
+          if (artistData) {
+            const mappedArtist = {
+              name: artistData?.Name?.[0] || '',
+              spotifyProfile: artistData?.SpotifyProfile?.[0] || '',
+              facebookProfile: artistData?.FacebookProfile?.[0] || '',
+              instagramProfile: artistData?.InstagramProfile?.[0] || '',
+              twitterProfile: artistData?.TwitterProfile?.[0] || '',
+              mail: artistData?.Mail?.[0] || ''
+            };
+            setArtist(mappedArtist);
+          }
+        }
+      });
+    } catch (error) {
+      console.error('Error fetching XML:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchXmlData();
+  }, []);
+
+  if (!artist) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <div>
       <div className='top_div'>
-        <h1 className='main_title'>G  A  B  I.</h1>
-
-        {/* <div className='arrow-container' onClick={() => scrollToBottom()}>
-          <FontAwesomeIcon icon={faCircleDown} className='arrowDown' />
-        </div> */}
+        <h1 className='main_title'>{artist.name}</h1>
         <div className='containerCenter'>
           <div className='LinkContainer'>
-            <a href="" className='links'><FontAwesomeIcon icon={faFacebook} beat /></a>
-            <a href="" className='links'><FontAwesomeIcon icon={faTwitter} beat /></a>
-            <a href="https://www.instagram.com/Nineteen.ninety.v" className='links'><FontAwesomeIcon icon={faInstagram} beat /></a>
-            <a href="" className='links'><FontAwesomeIcon icon={faEnvelope} beat /></a>
-            <a href="https://open.spotify.com/artist/7GJYItBw6A5skqnKs9Bbrz" className='links'><FontAwesomeIcon icon={faSpotify} beat /></a>
+            <a href={artist.facebookProfile} className='links'>
+              <FontAwesomeIcon icon={faFacebook} beat />
+            </a>
+            <a href={artist.twitterProfile} className='links'>
+              <FontAwesomeIcon icon={faTwitter} beat />
+            </a>
+            <a href={artist.instagramProfile} className='links'>
+              <FontAwesomeIcon icon={faInstagram} beat />
+            </a>
+            <a href={`mailto:${artist.mail}`} className='links'>
+              <FontAwesomeIcon icon={faEnvelope} beat />
+            </a>
+            <a href={artist.spotifyProfile} className='links'>
+              <FontAwesomeIcon icon={faSpotify} beat />
+            </a>
           </div>
         </div>
       </div>
       <AlbumList />
     </div>
-  )
-}
+  );
+};
